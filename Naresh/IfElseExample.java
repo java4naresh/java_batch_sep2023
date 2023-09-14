508c41776959028bc0eecb42dcae71bf1cbd037c
java4naresh <kambalanaresh4java@gmail.com>
2023-09-14 08:02:11 +0530

    conditional statements

---
+++ Naresh/IfElseExample.java
@@ -0,0 +1,20 @@
+class IfElseExample 
+{
+	public static void main(String[] args) 
+	{
+		int num1 = 5;
+		int num2 = 0;
+
+		if(num1%2==0) {
+		System.out.println("Given number is Even");
+		System.out.println("Your given number is not zero!");
+	    } else
+		System.out.println("Given number is Odd");
+
+		if(num2<0) {
+		System.out.println("Given number is Negitive");
+		} else {
+		System.out.println("Given number is Positive");
+		}
+	}
+}
---
+++ Naresh/IfExample.java
@@ -0,0 +1,21 @@
+class IfExample
+{
+	public static void main(String[] args) 
+	{
+		int num = 1;
+		int num2 = 6;
+		int num3 = -5;
+		if(num!=0) 
+		System.out.println("Your given number is not zero!");
+		
+
+		if(num2%2==0) {
+		System.out.println("Your given number is Even!");
+		}
+
+        if(num3<0) {
+		System.out.println("Your given number is Negitive!");
+		}
+
+	}
+}

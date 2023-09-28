629a103448767acf535601768e428213b9b3f23b
java4naresh <kambalanaresh4java@gmail.com>
2023-09-28 21:02:47 +0530

    first object class

---
+++ Naresh/corejava/src/org/naresh/corejava/classobject/Human.java
@@ -0,0 +1,64 @@
+package org.naresh.corejava.classobject;
+
+//import statements
+
+public class Human {
+	
+	String name;
+	char gender;
+	int age;
+	double height;
+	double weight;
+	String color;
+	
+	public void awakeTimeAndPrint(double wakeupTime) {
+		System.out.println(name+" will wake up at "+wakeupTime+" regulary");
+	}
+	
+	public static void main(String[] args) {
+		Human naresh = new Human();
+		/*System.out.println(naresh.name);
+		System.out.println(naresh.gender);
+		System.out.println(naresh.age);
+		System.out.println(naresh.height);
+		System.out.println(naresh.weight);
+		System.out.println(naresh.color);
+		System.out.println("====================");*/
+	    naresh.name="Naresh";
+		naresh.gender='M';
+		naresh.age=30;
+		naresh.height=5.8;
+		naresh.weight=75;
+		naresh.color="white";
+		naresh.awakeTimeAndPrint(6.25);//calling
+		/*System.out.println(naresh.name);
+		System.out.println(naresh.gender);
+		System.out.println(naresh.age);
+		System.out.println(naresh.height);
+		System.out.println(naresh.weight);
+		System.out.println(naresh.color);
+		System.out.println("====================");*/
+		Human surya = new Human();
+		/*System.out.println(surya.name);
+		System.out.println(surya.gender);
+		System.out.println(surya.age);
+		System.out.println(surya.height);
+		System.out.println(surya.weight);
+		System.out.println(surya.color);
+		System.out.println("====================");*/
+		surya.name="Surya";
+		surya.age = 50;
+		surya.color = "White";
+		surya.gender = 'M';
+		surya.height = 5.6;
+		surya.weight = 80.5;
+		surya.awakeTimeAndPrint(6);
+		/*System.out.println(surya.name);
+		System.out.println(surya.gender);
+		System.out.println(surya.age);
+		System.out.println(surya.height);
+		System.out.println(surya.weight);
+		System.out.println(surya.color);*/
+	}
+
+}

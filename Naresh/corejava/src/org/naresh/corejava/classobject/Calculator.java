294e6b106357b145348157f3e7e7c235fe63d33d
java4naresh <kambalanaresh4java@gmail.com>
2023-10-03 20:58:32 +0530

    static example

--- Naresh/corejava/src/org/naresh/corejava/classobject/Calculator.java
+++ Naresh/corejava/src/org/naresh/corejava/classobject/Calculator.java
@@ -2,11 +2,11 @@ package org.naresh.corejava.classobject;
 
 public class Calculator {
 	
-	public int sum(int num1, int num2) {
+	public static int sum(int num1, int num2) {
 		return num1+num2;
 	}
 	
-	public int sumOfMyChoice(int... numbers) {
+	public static int sumOfMyChoice(int... numbers) {
 		int sum = 0;
 		for(int number:numbers) {
 			sum = sum + number;
@@ -14,7 +14,7 @@ public class Calculator {
 		return sum;
 	}
 	
-	public int mulOfMyChoice(int... numbers) {
+	public static int mulOfMyChoice(int... numbers) {
 		int mul = 1;
 		for(int number:numbers) {
 			mul = mul * number;
@@ -24,10 +24,10 @@ public class Calculator {
 	}
 	
 	public static void main(String[] args) {
-		Calculator c = new Calculator();
+		//Calculator c = new Calculator();
 		//c.sum(10, 20);
-		System.out.println(c.sumOfMyChoice(10,20,30,40,50));
-		System.out.println(c.mulOfMyChoice(1,2,3,4,5));
+		System.out.println(Calculator.sumOfMyChoice(10,20,30,40,50));
+		System.out.println(Calculator.mulOfMyChoice(1,2,3,4,5));
 	}
 
 }
--- Naresh/corejava/src/org/naresh/corejava/classobject/Customer.java
+++ Naresh/corejava/src/org/naresh/corejava/classobject/Customer.java
@@ -5,8 +5,23 @@ public class Customer {
 	String name;
 	char gender;
 	int age;
+	static String bankName;
+	static String mainBranchAddress;
+	static String bankCode;
 	
-	/*public Customer(String nam, char gend, int ag) {
+	static {
+		System.out.println("static");
+		mainBranchAddress = "Mumbai";
+		bankName = "HDFC Bank";
+		bankCode = "HDFC";
+	}
+	
+	public static void printChairmanName() {
+		System.out.println("Naresh");
+	}
+	
+	public Customer(String nam, char gend, int ag) {
+		System.out.println("cons");
 		name = nam;
 		gender = gend;
 		age = ag;
@@ -19,39 +34,35 @@ public class Customer {
 	public Customer(String nam, char gend) {
 		name = nam;
 		gender = gend;
-	}*/
+	}
 	
 	
 	
 	public static void main(String[] args) {
+		Customer.printChairmanName();
+		System.out.println("main");
 		//Customer c = new Customer("Naresh", 'm', 30);
-		Customer c = new Customer();
+		Customer c = new Customer("Naresh", 'M', 30);
 		//Customer c = new Customer("Naresh", 'M');
 		//System.out.println(c.name);//
 		//System.out.println(c.gender);//
 		//System.out.println(c.age);//
-		System.out.println(c);
-	}
-	
-	
-
-	public Customer(String name, char gender) {
-		this.name = name;
-		this.gender = gender;
-	}
-
-	public Customer() {
+		//System.out.println(c);
+		Customer c2 = new Customer("Nikita", 'F', 20);
+		//System.out.println(c2);
+		
+		Customer c3 = new Customer("Surya", 'M', 50);
+		//System.out.println(c3);
+		//Customer.bankCode = "HC";
+		//System.out.println(c);
+		//System.out.println(c2);
+		//System.out.println(c3);
 		
 	}
 
-	public Customer(String name, char gender, int age) {
-		this.name = name;
-		this.gender = gender;
-		this.age = age;
-	}
-
+	@Override
 	public String toString() {
-		return "CustomerDetails[name="+name+", gender="+gender+", age="+age+"]";
+		return "Customer [name=" + name + ", gender=" + gender + ", age=" + age + ", mainBranchAddress="+mainBranchAddress+", bankName="+bankName+", bankCode="+bankCode+"]";
 	}
 	
 	

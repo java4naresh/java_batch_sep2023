32d290899c35ddf0e5f102a82feb209fe187aeff
java4naresh <kambalanaresh4java@gmail.com>
2023-10-30 20:33:26 +0530

    exception handling

--- Naresh/corejava/src/org/naresh/corejava/exceptionhandling/ExceptionHandlingDemo.java
+++ Naresh/corejava/src/org/naresh/corejava/exceptionhandling/ExceptionHandlingDemo.java
@@ -1,21 +1,35 @@
 package org.naresh.corejava.exceptionhandling;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class ExceptionHandlingDemo {
 
-	public static void main(String[] args) {
-		ExceptionHandlingDemo.division(10, 0);
+	public static void main(String[] args) throws FileNotFoundException {
+		/*ExceptionHandlingDemo.division(10, 0);
 		ExceptionHandlingDemo.checkValue("Naresh", "Kambala");
 		int[] arr = {1,2,3,4,5};
 		//ExceptionHandlingDemo.getIndexValue(new int[] {1,2,3,4,5}, 2);
 		ExceptionHandlingDemo.getIndexValue(arr, 2);
 		ExceptionHandlingDemo.getStringIndexValue("Naresh", 0);
+		String fileContent = ExceptionHandlingDemo.readFile();
+		ExceptionHandlingDemo.writeFile(fileContent);*/
+		
+		ExceptionHandlingDemo.checkEmailFormat("java4naresh@gmail");
 
 	}
 	
+	private static void checkEmailFormat(String email) {
+		if(!email.matches("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[a-z]{2,6}"))
+			throw new RuntimeException("Email is not correct");
+		
+	}
+
 	public static void division(int num1, int num2) {
 		try {
 		System.out.println(num1/num2);
-		} catch(ArithmeticException e) {
+		}catch(ArithmeticException e) {
 			//e.printStackTrace();
 			//System.out.println(e.getMessage());
 			System.out.println(e.toString());
@@ -51,13 +65,37 @@ public class ExceptionHandlingDemo {
 	}
 	
 	public static void getStringIndexValue(String value, int index) {
-		try {
+		   try {
 			System.out.println(value.charAt(index));
 			} catch(Exception e) {
 				e.printStackTrace();
 			} finally {
 				System.out.println("Exception handled");
 			}
+	}
+	
+	public static String readFile() throws FileNotFoundException {
+		
+		File file = new File("sample.txt");
+		FileReader fr = new FileReader(file);
+		
+		return "Hello World!";
+		
+	}
+	
+	public static void readFile2()  {
+		try {
+		File file = new File("sample.txt");
+		FileReader fr = new FileReader(file);
+		} catch(FileNotFoundException fne) {
+			
+		} finally {
+			
+		}
+	}
+	
+	public static void writeFile(String content) {
+		
 	}
 
 }

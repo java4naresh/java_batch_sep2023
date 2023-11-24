d7084dab557b3a38022873a65730c39cd4bd723a
java4naresh <kambalanaresh4java@gmail.com>
2023-11-24 20:28:25 +0530

    LinkedList and Vector Examples

--- Naresh/corejava/src/org/naresh/corejava/collectionframework/ListExamples.java
+++ Naresh/corejava/src/org/naresh/corejava/collectionframework/ListExamples.java
@@ -1,13 +1,72 @@
 package org.naresh.corejava.collectionframework;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
 
 public class ListExamples {
 	
 	public static void main(String[] args) {
-		genericArrayList();
+		genericVector();
 		
+	}
+	
+	public static void genericVector() {
+		List<Character> characters = new Vector<>(); 
+		characters.add('A');
+		characters.add('E');
+		characters.add('I');
+		characters.add('O');
+		characters.add('U');
+		characters.add('a');
+		characters.add('e');
+		characters.add('i');
+		characters.add('o');
+		characters.add('u');
+		List<Character> subList = characters.subList(5, characters.size());
+		System.out.println(subList);
+		Object[] array = characters.toArray();
+		System.out.println(Arrays.toString(array));
+		List<String> listArray = Arrays.asList(new String[]{"naresh", "nikita"});
+		System.out.println(listArray);
+		/*char check = 'A';
+		if(characters.contains(check)) {
+			System.out.println("Owel");
+		} else {
+			System.out.println("Consonent");
+		}*/
+	}
+	
+	public static void genericLinkedList() {
+		LinkedList<String> names = new LinkedList<>();
+		names.add("Rohit");
+		names.add("Kohli");
+		names.add("Gill");
+		names.add("Rahul");
+		names.add("Shreyas");
+		names.add("Hardik");
+		names.add("Jadeja");
+		names.add("Shami");
+		names.add("Bumra");
+		names.add("Siraj");
+		names.add("Kuldeep");
+		System.out.println(names.get(0));
+		names.remove("Hardik");
+		names.add(5, "Surya");
+		System.out.println(names);
+		List<String> extraPlayers = new ArrayList<>();
+		extraPlayers.add("Ishan");
+		extraPlayers.add("Ashwin");
+		extraPlayers.add("Shardul Takur");
+		extraPlayers.add("Hardik");
+		names.addAll(extraPlayers);
+		System.out.println(names.getFirst());
+		System.out.println(names.getLast());
+		System.out.println(names.size());
+		System.out.println(names.indexOf("Naresh"));
 	}
 	
 	public static void genericArrayList() {

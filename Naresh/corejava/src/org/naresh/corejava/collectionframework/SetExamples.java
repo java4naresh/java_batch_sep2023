6bcf15a53d2a18f3f9981d72590eceddfab4c6c7
java4naresh <kambalanaresh4java@gmail.com>
2023-12-08 21:05:49 +0530

    custom objects in set

--- Naresh/corejava/src/org/naresh/corejava/collectionframework/SetExamples.java
+++ Naresh/corejava/src/org/naresh/corejava/collectionframework/SetExamples.java
@@ -8,8 +8,28 @@ import java.util.TreeSet;
 public class SetExamples {
 
 	public static void main(String[] args) {
-		hashSetExamples();
+		customTreeSet();
 
+	}
+	
+	public static void customTreeSet() {
+		/*Set<String> names = new TreeSet<>();
+		names.add("Kohli");
+		System.out.println(names);
+		names.add("Rohit");
+		System.out.println(names);
+		names.add("Surya");
+		System.out.println(names);*/
+		//System.out.println("Nikita".compareTo("Naresh"));
+		System.out.println(new Integer(2).compareTo(new Integer(1)));
+		Set<Customer> customers = new TreeSet<>();
+		customers.add(new Customer(2, "Naresh"));
+		customers.add(new Customer(1, "Nikita"));
+		customers.add(new Customer(5, "Naresh"));
+		customers.add(new Customer(8, "Nikita"));
+		customers.add(new Customer(0, "Naresh"));
+		customers.add(new Customer(9, "Nikita"));
+		System.out.println(customers);
 	}
 	
 	public static void treeSetExample() {
@@ -49,6 +69,17 @@ public class SetExamples {
 		System.out.println(chars);
 	}
 	
+	public static void customLinkedHashSet() {
+		Set<Customer> customers = new LinkedHashSet<>();
+		customers.add(new Customer(1, "Naresh"));
+		customers.add(new Customer(2, "Nikita"));
+		customers.add(new Customer(1, "Naresh"));
+		customers.add(new Customer(2, "Nikita"));
+		customers.add(new Customer(1, "Naresh"));
+		customers.add(new Customer(2, "Nikita"));
+		System.out.println(customers);
+	}
+	
 	public static void linkedHashSet() {
 		Set<String> names = new LinkedHashSet<>();
 		names.add("Rohit");
@@ -70,20 +101,31 @@ public class SetExamples {
 		System.out.println(names);
 	}
 	
+	public static void customHashSet() {
+		Set<Customer> customers = new HashSet<>();
+		customers.add(new Customer(1, "Naresh"));
+		customers.add(new Customer(2, "Nikita"));
+		customers.add(new Customer(1, "Naresh"));
+		customers.add(new Customer(2, "Nikita"));
+		customers.add(new Customer(1, "Naresh"));
+		customers.add(new Customer(2, "Nikita"));
+		System.out.println(customers);
+	}
+	
 	public static void hashSetExamples() {
 		Set<String> names = new HashSet<>();
-		names.add("Rohit");
-		names.add("Kohli");
-		names.add("Gill");
-		names.add("Rahul");
-		names.add("Shreyas");
-		names.add("Hardik");
-		names.add("Jadeja");
-		names.add("Shami");
-		names.add("Bumra");
-		names.add("Siraj");
-		names.add("Kuldeep");
-		names.add("Rohit");
+		names.add(new String("Rohit"));
+		names.add(new String("Kohli"));
+		names.add(new String("Gill"));
+		names.add(new String("Rahul"));
+		names.add(new String("Shreyas"));
+		names.add(new String("Hardik"));
+		names.add(new String("Jadeja"));
+		names.add(new String("Shami"));
+		names.add(new String("Bumra"));
+		names.add(new String("Siraj"));
+		names.add(new String("Kuldeep"));
+		names.add(new String("Rohit"));
 		System.out.println(names);
 		//names.clear();
 		//names.remove("Rohit");
